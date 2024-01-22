1351d7f05e9ecd1c7258a1ed521898396dc817ab
Matt Matheus <matt.matheus@outlook.com>
2024-01-21 17:38:01 -0800

    Count!

--- juno/dailycount/models.py
+++ juno/dailycount/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class FoodItem(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+    calories = models.IntegerField()
+    mood = models.CharField(max_length=200, default="none")
--- juno/dailycount/views.py
+++ juno/dailycount/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .forms import FoodItemForm
 
 
 # Create your views here.
 def index(request):
-    return render(request, "dailycount/index.html")
+    form = FoodItemForm()
+    return render(request, "dailycount/index.html", {"form": form})
+
+
+def food_item_view(request):
+    form = FoodItemForm()
+    return render(request, "index.html", {"form": form})
